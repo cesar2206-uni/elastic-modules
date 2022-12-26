aef1eaaa133f49e9e25c5fc2da34a959ade93e39
César Manuel Sánchez Oré <csanchezo@uni.pe>
2022-12-26 14:48:53 -0500

    Correcciones de mPa, widgets y secante

--- app.py
+++ app.py
@@ -20,15 +20,16 @@ uploaded_file = st.sidebar.file_uploader("Choose a .csv file:")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, names = ["strain", "stress"])
     #string_value = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
-correction_0 = st.sidebar.checkbox('Correction in (0, 0)')
-
+correction_0 = st.sidebar.checkbox('Correction 1: XY in (0, 0)')
+correction_mpa = st.sidebar.checkbox('Correction 2: Y axis in mPa')
 st.sidebar.markdown("# Secant elastic modulus")
 
 correction_secant = st.sidebar.checkbox('Correction of Secant elastic modulus')
-d_secant = st.sidebar.slider('Initial displacement', 0.5, 2.0, 1.0)
+d_secant = st.sidebar.slider('Initial displacement', 0.0, 2.0, 1.0)
 
 st.sidebar.markdown("# Linear elastic modulus")
 fit_value = st.sidebar.number_input("Minimum fit lenght", min_value = 3, value = 7, step = 1)
+
 
 
 # Processing
@@ -47,7 +48,7 @@ def plot_modulus(fit_value):
         num_points = len(x)
         min_fit_length = fit_value
         chi = 0
-        chi_min = 10000
+        chi_min = 10000000
         i_best = 0
         j_best = 0
 
@@ -72,27 +73,41 @@ def plot_modulus(fit_value):
 
     # Calculation of tangent modulus
     EX_tan, EY_tan = chi_algorithm(strain, stress, fit_value)
-    E_tan = (EY_tan[1] - EY_tan[0])/(EX_tan[1] - EX_tan[0]) * 100/1000
-
+    if correction_mpa:
+        E_tan = (EY_tan[1] - EY_tan[0])/(EX_tan[1] - EX_tan[0]) * 100
+    else:
+        E_tan = (EY_tan[1] - EY_tan[0])/(EX_tan[1] - EX_tan[0]) * 100/1000
     # Obtain the secant stiffness modulus
     max_stress = max(stress)
     med_stress = max_stress / 2
 
     for i, stress_value in enumerate(stress):
-        if med_stress > stress[i] and med_stress < stress[i + 1]:
+        if med_stress >= stress[i] and med_stress <= stress[i + 1]:
             med_strain = strain[i] + (med_stress - stress[i]) * (strain[i + 1] - strain[i]) / (stress[i + 1] - stress[i])
             break
+    
+    if correction_secant:     
+        for i, strain_value in enumerate(strain):
+            if d_secant >= strain[i] and d_secant <= strain[i + 1]:
+                d_stress = stress[i] + (d_secant - strain[i]) * (stress[i + 1] - stress[i]) / (strain[i + 1] - strain[i])
+                break
 
     # Calculation of secant modulus
     
     if correction_secant:
         EX_sec = np.array([d_secant, med_strain])
-        EY_sec = np.array([0, med_stress])
-        E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000    
+        EY_sec = np.array([d_stress, med_stress])
+        if correction_mpa:
+            E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100
+        else:
+            E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000    
     else:
         EX_sec = np.array([0, med_strain])
         EY_sec = np.array([0, med_stress])
-        E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000
+        if correction_mpa:
+            E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100
+        else:
+            E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000
 
     # Plotting zone
     
@@ -147,8 +162,11 @@ def plot_modulus(fit_value):
     em_fig.suptitle('Ensayo de compresión no confinada')
     em_ax.set_title(uploaded_file.name[:-4])
     em_ax.set_xlabel("Deformación Axial, %", labelpad=10)
-    em_ax.set_ylabel('Esfuerzo a compresión, kPa', labelpad=10)
     
+    if correction_mpa:
+        em_ax.set_ylabel('Esfuerzo a compresión, mPa', labelpad=10)
+    else:
+        em_ax.set_ylabel('Esfuerzo a compresión, kPa', labelpad=10)
     
     return em_fig
 
