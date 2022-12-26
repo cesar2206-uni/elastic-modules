ac1cf61f7bdc9e11765c82b0ee98d7de6bcc542f
César Manuel Sánchez Oré <csanchezo@uni.pe>
2022-12-26 12:38:00 -0500

    Modulo Secante listo

--- app.py
+++ app.py
@@ -21,6 +21,16 @@ if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, names = ["strain", "stress"])
     #string_value = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
 correction_0 = st.sidebar.checkbox('Correction in (0, 0)')
+
+st.sidebar.markdown("# Secant elastic modulus")
+
+correction_secant = st.sidebar.checkbox('Correction of Secant elastic modulus')
+d_secant = st.sidebar.slider('Initial displacement', 0.5, 2.0, 1.0)
+
+st.sidebar.markdown("# Linear elastic modulus")
+fit_value = st.sidebar.number_input("Minimum fit lenght", min_value = 3, value = 7, step = 1)
+
+
 # Processing
    
 strain = df["strain"].to_numpy()
@@ -30,12 +40,12 @@ if correction_0:
     strain = np.concatenate((np.array([0]), df["strain"].to_numpy()))
     stress = np.concatenate((np.array([0]), df["stress"].to_numpy()))
 
-def plot_modulus():
+def plot_modulus(fit_value):
     
     # Algorithm of chi square
-    def chi_algorithm(x, y):
+    def chi_algorithm(x, y, fit_value):
         num_points = len(x)
-        min_fit_length = 7
+        min_fit_length = fit_value
         chi = 0
         chi_min = 10000
         i_best = 0
@@ -61,7 +71,7 @@ def plot_modulus():
         return x[i_best:j_best], y_linear
 
     # Calculation of tangent modulus
-    EX_tan, EY_tan = chi_algorithm(strain, stress)
+    EX_tan, EY_tan = chi_algorithm(strain, stress, fit_value)
     E_tan = (EY_tan[1] - EY_tan[0])/(EX_tan[1] - EX_tan[0]) * 100/1000
 
     # Obtain the secant stiffness modulus
@@ -74,9 +84,15 @@ def plot_modulus():
             break
 
     # Calculation of secant modulus
-    EX_sec = np.array([0, med_strain])
-    EY_sec = np.array([0, med_stress])
-    E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000
+    
+    if correction_secant:
+        EX_sec = np.array([d_secant, med_strain])
+        EY_sec = np.array([0, med_stress])
+        E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000    
+    else:
+        EX_sec = np.array([0, med_strain])
+        EY_sec = np.array([0, med_stress])
+        E_sec = (EY_sec[1] - EY_sec[0])/(EX_sec[1] - EX_sec[0]) * 100/1000
 
     # Plotting zone
     
@@ -132,14 +148,26 @@ def plot_modulus():
     em_ax.set_title(uploaded_file.name[:-4])
     em_ax.set_xlabel("Deformación Axial, %", labelpad=10)
     em_ax.set_ylabel('Esfuerzo a compresión, kPa', labelpad=10)
-
+    
+    
     return em_fig
 
 
 # Principal Layout
-st.markdown("# Plotting Evaluation")   
+st.markdown("# Evaluation of Elastic Modulus")   
 
 #if correction_0:
-em_result = plot_modulus()
+em_result = plot_modulus(fit_value)
 st.pyplot(em_result)
-    
+
+fn = uploaded_file.name[:-3]+ "png"
+plt.savefig(fn)
+with open(fn, "rb") as img:
+    btn = st.download_button(
+        label="Download image",
+        data=img,
+        file_name=fn,
+        mime="image/png"
+    )
+        
+st.markdown("Author: César Manuel Sánchez Oré (cesar.sanchez@wsp.com)")
